5fab53d7d0ea278f5f66c98a455691ebc4f47cc7
Arnest Allka <arisallkas@gmail.com>
2024-07-24 16:06:06 +0300

    Testing HTMLNode: success

---
+++ src/htmlnode.py
@@ -0,0 +1,28 @@
+class HTMLNode():
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value 
+        self.children = children
+        self.props = props
+        
+    def to_html(self):
+        raise NotImplementedError()
+    
+    def props_to_html(self):
+        props_str = ""
+        if self.props:
+            for key, value in self.props.items():
+                props_str += f'{key}="{value}" '
+        return props_str
+    
+    def __repr__(self):
+        result = "HTMLNode:\n"
+        if self.tag:
+            result += f"tag: {self.tag}\n"
+        if self.value:
+            result += f"value: {self.value}\n"
+        if self.children:
+            result += f"children: {self.value}\n"
+        if self.props:
+            result += f"props: {self.props}\n"
+        return result
---
+++ src/test_htmlnode.py
@@ -0,0 +1,43 @@
+import unittest
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_init_no_arguments(self):
+        node = HTMLNode()
+        self.assertIsNone(node.tag)
+        self.assertIsNone(node.value)
+        self.assertIsNone(node.children)
+        self.assertIsNone(node.props)
+
+    def test_init_with_arguments(self):
+        children = [HTMLNode(tag='p', value='child')]
+        props = {'class': 'main'}
+        node = HTMLNode(tag='div', value='parent', children=children, props=props)
+        self.assertEqual(node.tag, 'div')
+        self.assertEqual(node.value, 'parent')
+        self.assertEqual(node.children, children)
+        self.assertEqual(node.props, props)
+
+    def test_props_to_html_no_props(self):
+        node = HTMLNode()
+        self.assertEqual(node.props_to_html(), "")
+
+    def test_props_to_html_with_props(self):
+        props = {'class': 'main', 'id': 'unique'}
+        node = HTMLNode(props=props)
+        self.assertEqual(node.props_to_html(), 'class="main" id="unique" ')
+
+    def test_repr_no_arguments(self):
+        node = HTMLNode()
+        expected_repr = "HTMLNode:\n"
+        self.assertEqual(repr(node), expected_repr)
+
+    def test_repr_with_arguments(self):
+        children = [HTMLNode(tag='p', value='child')]
+        props = {'class': 'main'}
+        node = HTMLNode(tag='div', value='parent', children=children, props=props)
+        expected_repr = "HTMLNode:\ntag: div\nvalue: parent\nchildren: parent\nprops: {'class': 'main'}\n"
+        self.assertEqual(repr(node), expected_repr)
+
+if __name__ == '__main__':
+    unittest.main()
