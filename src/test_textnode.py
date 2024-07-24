721126175e99a483286357ba3d4cfe1450049739
Arnest Allka <arisallkas@gmail.com>
2024-07-24 13:10:30 +0300

    Testing TextNode

---
+++ src/test_textnode.py
@@ -0,0 +1,32 @@
+import unittest
+
+from textnode import TextNode
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode('This is a text node', 'bold')
+        node2 = TextNode('This is a text node', 'bold')
+        self.assertEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode('This is a text node', 'bold', 'http://example.com')
+        node2 = TextNode('This is a text node', 'bold', 'http://example.com')
+        self.assertEqual(node, node2)
+        
+    def test_eq_not_equal(self):
+        node = TextNode('This is a text node', 'bold')
+        node2 = TextNode('This is a text node', 'italic')
+        self.assertNotEqual(node, node2)
+        
+    def test_eq_url_not_equal(self):
+        node = TextNode('This is a text node', 'bold', 'http://example.com')
+        node2 = TextNode('This is a text node', 'bold', 'http://example.org')
+        self.assertNotEqual(node, node2)
+        
+    def test_eq_url_None(self):
+        node = TextNode('This is a text node', 'bold', None)
+        node2 = TextNode('This is a text node', 'bold', None)
+        self.assertEqual(node, node2)
+    
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,15 +1,25 @@
 class TextNode():
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
         
     def __eq__(self, other):
-        if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
-            return True
+        if self.text == other.text and self.text_type == other.text_type:
+            if self.url and other.url:
+                if self.url == other.url:
+                    return True
+                else:
+                    return False
+            else:
+                return True
         else:
             return False
-    
+        
+        
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+        if self.url:
+            return f"TextNode({self.text}, {self.text_type}, {self.url})"
+        else:
+            return f"TextNode({self.text}, {self.text_type})"
 
