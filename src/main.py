17d044bd755f9019acca9cb46dab83887a3153c3
Arnest Allka <arisallkas@gmail.com>
2024-07-30 15:58:53 +0300

    Copy static folder to public

--- src/main.py
+++ src/main.py
@@ -1,13 +1,28 @@
-from leafnode import LeafNode
-from parentnode import ParentNode
-from textnode import TextNode
+import os
+import shutil
+
+def copy_directory(src, dst):
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    os.mkdir(dst)
+
+    for item in os.listdir(src):
+        src_path = os.path.join(src, item)
+        dst_path = os.path.join(dst, item)
+
+        if os.path.isdir(src_path):
+            copy_directory(src_path, dst_path)
+        else:
+            shutil.copy(src_path, dst_path)
+            print(f"Copied file: {src_path}")
 
 def main():
-    test_list = [1,2]
+    source_dir = 'static'
+    public_dir = 'public'
     
-    if test_list:
-        print('True')
-    else:
-        print('False')
+    copy_directory(source_dir, public_dir)
+    print("Directory copy completed.")
+            
+if __name__ == '__main__':
+    main()
     
-main()
