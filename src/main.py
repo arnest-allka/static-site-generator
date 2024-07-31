1ad99da8175a045bfcebcd9a94b29738a71b233a
Arnest Allka <arisallkas@gmail.com>
2024-07-31 13:55:56 +0300

    Functions that generate the html files

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import os
 import re
 import shutil
 
+from block_markdown import markdown_to_html_node
+
 def copy_directory(src, dst):
     if os.path.exists(dst):
         shutil.rmtree(dst)
@@ -25,12 +27,56 @@ def extract_title(markdown):
     else:
         raise Exception("No h1 header found")
 
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    
+    with open(from_path, 'r') as file:
+        markdown_content = file.read()
+
+    with open(template_path, 'r') as file:
+        template_content = file.read()
+
+    html_node = markdown_to_html_node(markdown_content)
+    html_content = html_node.to_html()
+
+    title = extract_title(markdown_content)
+
+    full_html = template_content.replace("{{ Title }}", title).replace("{{ Content }}", html_content)
+
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+
+    with open(dest_path, 'w') as file:
+        file.write(full_html)
+    
+    print(f"Page generated: {dest_path}")
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.makedirs(dest_dir_path)
+    
+    for item in os.listdir(dir_path_content):
+        src_path = os.path.join(dir_path_content, item)
+        dst_path = os.path.join(dest_dir_path, item)
+
+        if os.path.isdir(src_path):
+            generate_pages_recursive(src_path, template_path, dst_path)
+        elif src_path.endswith(".md"):
+            dst_path = dst_path.replace(".md", ".html")
+            generate_page(src_path, template_path, dst_path)
+
+
 def main():
     source_dir = 'static'
     public_dir = 'public'
     
     copy_directory(source_dir, public_dir)
     print("Directory copy completed.")
-            
+    
+    markdown_path = 'content'
+    template_path = 'template.html'
+    dest_path = 'public'
+
+    generate_pages_recursive(markdown_path, template_path, dest_path)
+  
 if __name__ == '__main__':
     main()
